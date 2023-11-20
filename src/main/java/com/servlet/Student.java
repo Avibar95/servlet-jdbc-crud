6aa50af6bb2304cf929d98c76bb2f985555b7491
Avibar95 <barpeteavinash1995@gmail.com>
2023-11-20 21:53:09 +0530

    intial commit

---
+++ src/main/java/com/servlet/Student.java
@@ -0,0 +1,35 @@
+package com.servlet;
+
+public class Student {
+	
+	private String name,email;
+	private int rollnumber,contectnumber;
+	
+	
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public int getRollnumber() {
+		return rollnumber;
+	}
+	public void setRollnumber(int rollnumber) {
+		this.rollnumber = rollnumber;
+	}
+	public int getContectnumber() {
+		return contectnumber;
+	}
+	public void setContectnumber(int contectnumber) {
+		this.contectnumber = contectnumber;
+	}
+	
+
+}
